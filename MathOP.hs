38ece9690a14b4aaf9b0ad80782308a961a38013
obeliskgolem <obelisk.golem@gmail.com>
2019-08-27 17:51:00 +0800

    use readerT

--- MathOP.hs
+++ MathOP.hs
@@ -8,7 +8,6 @@ import Data.Aeson
 
 -- import Crypto.Hash.MD5
 import Data.Hashable
-import Data.ByteString.UTF8
 
 -- data IPAddress  = IPAddress String deriving (Generic, Show)
 -- data Port       = Int deriving (Generic, Show)
@@ -85,3 +84,65 @@ qualified n = if n < difficulty && n > 0
 
 qualifiedHash :: (Hashable a) => a -> Bool
 qualifiedHash k = qualified $ hash k
+
+--  pure functions          --
+verifyBlock :: Block -> Bool
+verifyBlock b = (merkelroot b == rootofmerkel (mkMerkelTree b)) && (qualifiedHash (mkHeader b))
+    where
+        merkelroot :: Block -> HashV
+        merkelroot block = mkMerkelRoot (mkHeader block)
+
+        rootofmerkel :: MerkelTree -> HashV
+        rootofmerkel (MerkelBranch root a b) = root
+        rootofmerkel _ = 0
+
+mining :: BlockHeader -> HashV -> Int -> BlockHeader
+mining phead mroot nonce = if calculate 
+    then constructBH 
+    else mining phead mroot (nonce + 1)
+    where 
+        constructBH :: BlockHeader
+        constructBH = BlockHeader {mkPrevHeader = phead, mkMerkelRoot = mroot, mkNonce = nonce}
+
+        calculate :: Bool
+        calculate = qualifiedHash constructBH 
+
+verifyTransaction :: Transaction -> [TxOutput] -> Bool
+verifyTransaction Transaction{mkInputs=txin, mkOutputs=txout} utxo = checkUTXO && checkAmount
+    where
+        checkAmount = ((foldl (+) 0 (map mkInAmount txin)) - (foldl (+) 0 (map mkOutAmount txout))) >= 0
+
+        checkUTXO = foldl (&&) True (map (`seekUTXO` utxo) txin)
+
+        seekUTXO :: TxInput -> [TxOutput] -> Bool
+        seekUTXO tin (x:xs) = if (mkInAddr tin == mkOutAddr x) && (mkInAmount tin == mkOutAmount x)
+            then True
+            else seekUTXO tin xs
+        seekUTXO _ [] = False
+
+generateMerkelTree :: [Transaction] -> MerkelTree
+generateMerkelTree tx = genMTree (map (MerkelLeaf . hash) tx)
+    where
+        genMTree :: [MerkelTree] -> MerkelTree
+        genMTree [] = MerkelLeaf (hash ([] :: [MerkelTree]))
+        genMTree (x:[]) = x
+        genMTree xs = genMTree (genPairs xs)
+
+        genPairs :: [MerkelTree] -> [MerkelTree]
+        genPairs (a:b:xs) = (MerkelBranch (hash (hash a + hash b)) a b : genPairs xs)
+        genPairs (x:[]) = [MerkelBranch (hash (hash x + hash x)) x x]
+        genPairs [] = []
+
+--  test data               --
+testInput1 = TxInput{mkInAddr = "Shanghai", mkInAmount = 10}
+testInput2 = TxInput{mkInAddr = "Guangzhou", mkInAmount = 20}
+testInput3 = TxInput{mkInAddr = "Beijing", mkInAmount = 50}
+
+testOutput1 = TxOutput{mkOutAddr = "Shanghai", mkOutAmount = 10}
+testOutput2 = TxOutput{mkOutAddr = "Beijing", mkOutAmount = 20}
+testOutput3 = TxOutput{mkOutAddr = "Guangzhou", mkOutAmount = 30}
+
+testTransaction =   [Transaction{mkInputs = [testInput1], mkOutputs = [testOutput1]}
+                    , Transaction{mkInputs = [testInput2], mkOutputs = [testOutput2]}
+                    , Transaction{mkInputs = [testInput3], mkOutputs = [testOutput3]}
+                    ]
--- Node.hs
+++ Node.hs
@@ -1,14 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Node where
 
 import MathOP
 import Data.Hashable
+import Data.IORef
+
+import Control.Monad.Reader
+import Control.Monad
 
 
 --  global data         --
 nodeConfig :: String
 nodeConfig = "node.config"
+
+data Env = Env {
+    eServerPort :: Int
+    ,   eNodes :: IORef [Node]
+    ,   eBlocks :: IORef [Block]
+    ,   eTransactions :: IORef [Transaction]
+    ,   eUTXO :: IORef [TxOutput]
+} 
+
+newtype AppT m a 
+    = AppT 
+    { unAppT :: ReaderT Env m a 
+    } deriving (Functor, Applicative, Monad)
 
 
 -- genesisBlockHeader :: BlockHeader
@@ -17,95 +36,34 @@ nodeConfig = "node.config"
 -- genesisBlock :: Block
 -- genesisBlock = Block {mkHeader = genesis}
 
---  test data           --
-
-
---  date kept in a node --
--- nodeList :: IORef [Node]
-
--- blockList :: IORef [Block]
-
--- incommingTransaction :: IORef [Transaction]
-
--- unspentMoney :: IORef [TxOutput]
-
-
---  pure functions          --
-verifyBlock :: Block -> Bool
-verifyBlock b = (merkelroot b == rootofmerkel (mkMerkelTree b)) && (qualifiedHash (mkHeader b))
-    where
-        merkelroot :: Block -> HashV
-        merkelroot block = mkMerkelRoot (mkHeader block)
-
-        rootofmerkel :: MerkelTree -> HashV
-        rootofmerkel (MerkelBranch root a b) = root
-        rootofmerkel _ = 0
-
-mining :: BlockHeader -> HashV -> Int -> BlockHeader
-mining phead mroot nonce = if calculate 
-    then constructBH 
-    else mining phead mroot (nonce + 1)
-    where 
-        constructBH :: BlockHeader
-        constructBH = BlockHeader {mkPrevHeader = phead, mkMerkelRoot = mroot, mkNonce = nonce}
-
-        calculate :: Bool
-        calculate = qualifiedHash constructBH 
-
-verifyTransaction :: Transaction -> [TxOutput] -> Bool
-verifyTransaction Transaction{mkInputs=txin, mkOutputs=txout} utxo = checkUTXO && checkAmount
-    where
-        checkAmount = ((foldl (+) 0 (map mkInAmount txin)) - (foldl (+) 0 (map mkOutAmount txout))) >= 0
-
-        checkUTXO = foldl (&&) True (map (`seekUTXO` utxo) txin)
-
-        seekUTXO :: TxInput -> [TxOutput] -> Bool
-        seekUTXO tin (x:xs) = if (mkInAddr tin == mkOutAddr x) && (mkInAmount tin == mkOutAmount x)
-            then True
-            else seekUTXO tin xs
-        seekUTXO _ [] = False
-
-generateMerkelTree :: [Transaction] -> MerkelTree
-generateMerkelTree tx = genMTree (map (MerkelLeaf . hash) tx)
-    where
-        genMTree :: [MerkelTree] -> MerkelTree
-        genMTree [] = MerkelLeaf (hash ([] :: [MerkelTree]))
-        genMTree (x:[]) = x
-        genMTree xs = genMTree (genPairs xs)
-
-        genPairs :: [MerkelTree] -> [MerkelTree]
-        genPairs (a:b:xs) = (MerkelBranch (hash (hash a + hash b)) a b : genPairs xs)
-        genPairs (x:[]) = [MerkelBranch (hash (hash x + hash x)) x x]
-        genPairs [] = []
-
 --  dealing with mutable data       --
---  initRegister :: IO ()
-
-requestBlock :: [Node] -> IO ()
-requestBlock nodes = do
-    
-
--- requestFullChain :: Node -> IO ()
+initRegister :: (MonadReader Env m, MonadIO m) => m ()
+initRegister = do
+    -- env <- ask
+    -- eServerPort env 
+    liftIO $ putStrLn "registered"
 
 -- respondRequest :: Node -> IO ()
 
 -- broadcastNode :: [Node] -> IO ()
 
---  test data               --
-testInput1 = TxInput{mkInAddr = "Shanghai", mkInAmount = 10}
-testInput2 = TxInput{mkInAddr = "Guangzhou", mkInAmount = 20}
-testInput3 = TxInput{mkInAddr = "Beijing", mkInAmount = 50}
-
-testOutput1 = TxOutput{mkOutAddr = "Shanghai", mkOutAmount = 10}
-testOutput2 = TxOutput{mkOutAddr = "Beijing", mkOutAmount = 20}
-testOutput3 = TxOutput{mkOutAddr = "Guangzhou", mkOutAmount = 30}
-
-testTransaction =   [Transaction{mkInputs = [testInput1], mkOutputs = [testOutput1]}
-                    , Transaction{mkInputs = [testInput2], mkOutputs = [testOutput2]}
-                    , Transaction{mkInputs = [testInput3], mkOutputs = [testOutput3]}
-                    ]
 
 --  serving as node         --
 runNode :: Int -> IO ()
 runNode port  = do
     putStrLn $ "bitcoin-h node running at port " ++ (show port)
+
+    nodes <- newIORef ([] :: [Node])
+    blocks <- newIORef ([] :: [Block])
+    tx <- newIORef ([] :: [Transaction])
+    utxo <- newIORef ([] :: [TxOutput])
+
+    let env = Env {
+        eServerPort = 19900
+        , eNodes = nodes
+        , eBlocks = blocks
+        , eTransactions = tx
+        , eUTXO = utxo
+    }
+
+    initRegister
